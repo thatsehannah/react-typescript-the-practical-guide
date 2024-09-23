1e462d3a151403b838e97d9a63cece7b09bce575
E. Hannah <echannah631@gmail.com>
2024-09-23 10:57:57 -0500

    stubbed out course goal component

---
+++ react-ts-basics/src/components/CourseGoal.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CourseGoal = () => {
+  return (
+    <article>
+      <div>
+        <h2>TITLE</h2>
+        <p>DESCRIPTION</p>
+      </div>
+      <button>delete</button>
+    </article>
+  );
+};
+
+export default CourseGoal;
