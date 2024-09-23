5c67f81753ae883f737ee7142df91cac935547ca
E. Hannah <echannah631@gmail.com>
2024-09-23 17:34:06 -0500

    began implementing useState

--- react-ts-basics/src/App.tsx
+++ react-ts-basics/src/App.tsx
@@ -1,19 +1,32 @@
+import { useState } from 'react';
 import './App.css';
 import CourseGoal from './components/CourseGoal';
 import Header from './components/Header';
 import goalsImg from './assets/goals.jpg';
 
-function App() {
+interface CourseGoal {
+  title: string;
+  description: string;
+  id: number;
+}
+
+const App = () => {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+  const handleAddGoal = () => {
+    setGoals();
+  };
+
   return (
     <main>
       <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
         <h1>Your Course Goals</h1>
       </Header>
+      <button onClick={handleAddGoal}>Add Goal</button>
       <CourseGoal title='Learn React + TS'>
         <p>Learn it from the ground up</p>
       </CourseGoal>
     </main>
   );
-}
+};
 
 export default App;
