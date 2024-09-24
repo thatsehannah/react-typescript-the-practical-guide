141ce76d905052ed727dee838f75f042662bc980
E. Hannah <echannah631@gmail.com>
2024-09-24 13:10:17 -0500

    outputting state-based values, created dummy data to add to list for now

--- react-ts-basics/src/App.tsx
+++ react-ts-basics/src/App.tsx
@@ -12,8 +12,16 @@ interface CourseGoal {
 
 const App = () => {
   const [goals, setGoals] = useState<CourseGoal[]>([]);
+
   const handleAddGoal = () => {
-    setGoals();
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: 'Learn React + TS',
+        description: 'Learn it in depth!',
+      };
+      return [...prevGoals, newGoal];
+    });
   };
 
   return (
@@ -22,9 +30,15 @@ const App = () => {
         <h1>Your Course Goals</h1>
       </Header>
       <button onClick={handleAddGoal}>Add Goal</button>
-      <CourseGoal title='Learn React + TS'>
-        <p>Learn it from the ground up</p>
-      </CourseGoal>
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal title={goal.title}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 };
