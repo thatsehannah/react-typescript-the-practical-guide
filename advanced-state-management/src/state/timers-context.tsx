8dfaa9a875f9714dc832ad2c4ab8255a1a03a4cb
E. Hannah <echannah631@gmail.com>
2024-10-10 12:08:38 -0500

    created a timers context

---
+++ advanced-state-management/src/state/timers-context.tsx
@@ -0,0 +1,21 @@
+import { createContext } from 'react';
+
+type Timer = {
+  name: string;
+  duration: number;
+};
+
+type TimersState = {
+  isRunning: boolean;
+  timers: Timer[];
+};
+
+type TimersContextType = TimersState & {
+  addTimer: (timerData: Timer) => void;
+  startTimers: () => void;
+  stopTimers: () => void;
+};
+
+const TimersContext = createContext<TimersContextType | null>(null);
+
+export default TimersContext;
