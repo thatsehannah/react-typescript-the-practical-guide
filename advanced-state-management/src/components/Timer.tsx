39cb37d49fca36e1cf50f6920b2bad06213550f5
E. Hannah <echannah631@gmail.com>
2024-10-12 17:21:37 -0500

    created a timer using useEffect in Timer component

--- advanced-state-management/src/components/Timer.tsx
+++ advanced-state-management/src/components/Timer.tsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from 'react';
 import { Timer as TimerProps } from '../types/Timer.ts';
 import Container from './ui/Container.tsx';
 
 const Timer = ({ name, duration }: TimerProps) => {
+  const [remainingTime, setRemainingTime] = useState<number>(duration * 1000); //converts duration to milliseconds
+
+  useEffect(() => {
+    setInterval(() => {
+      setRemainingTime((prevTime) => prevTime - 50);
+    }, 50);
+  }, []);
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
   return (
     <Container as='article'>
       <h2>{name}</h2>
-      <p>Duration: {duration} seconds</p>
+      <p>
+        <progress
+          max={duration * 1000}
+          value={remainingTime}
+        ></progress>
+      </p>
+      <p>{formattedRemainingTime}</p>
     </Container>
   );
 };
