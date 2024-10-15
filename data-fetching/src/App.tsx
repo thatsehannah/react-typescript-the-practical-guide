3226e2f48ee7d80965ee33eb610b6cb216b698af
E. Hannah <echannah631@gmail.com>
2024-10-15 12:13:10 -0500

    creating error checking, refactored what should be returned based on state

--- data-fetching/src/App.tsx
+++ data-fetching/src/App.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import { z } from 'zod';
 import { get } from './util/http';
 import { BlogPost } from './types/BlogPost';
 import BlogPosts from './components/BlogPosts';
 import fetchingImg from './assets/data-fetching.png';
+import ErrorMessage from './components/ErrorMessage';
 
 //Creating a schema for the data that's coming back from the API
 const rawDataBlogPostSchema = z.object({
@@ -16,42 +17,59 @@ const rawDataBlogPostSchema = z.object({
 const expectedResponseDataSchema = z.array(rawDataBlogPostSchema);
 
 const App = () => {
-  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>([]);
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
   const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState<string>();
 
   useEffect(() => {
     const fetchPosts = async () => {
       setIsFetching(true);
-      const data = await get('https://jsonplaceholder.typicode.com/posts');
+      try {
+        const data = await get('https://jsonplaceholder.typicode.com/posts');
+        const parsedData = expectedResponseDataSchema.parse(data); //will be the type set up in rawDataBlogPostSchema
 
-      const parsedData = expectedResponseDataSchema.parse(data); //will be the type set up in rawDataBlogPostSchema
-
-      //Transforming raw data to blog post type
-      const blogPosts: BlogPost[] = parsedData.map((rawPost) => {
-        return {
-          id: rawPost.id,
-          title: rawPost.title,
-          text: rawPost.body,
-        };
-      });
+        //Transforming raw data to blog post type
+        const blogPosts: BlogPost[] = parsedData.map((rawPost) => {
+          return {
+            id: rawPost.id,
+            title: rawPost.title,
+            text: rawPost.body,
+          };
+        });
+        setFetchedPosts(blogPosts);
+      } catch (e) {
+        if (e instanceof Error) {
+          setError(e.message);
+        }
+      }
 
       setIsFetching(false);
-      setFetchedPosts(blogPosts);
     };
 
     fetchPosts();
   }, []);
+
+  let content: ReactNode;
+
+  if (error) {
+    content = <ErrorMessage text={error} />;
+  }
+
+  if (fetchedPosts) {
+    content = <BlogPosts posts={fetchedPosts} />;
+  }
+
+  if (isFetching) {
+    content = <p id='loading-fallback'>Fetching posts...</p>;
+  }
+
   return (
     <main>
       <img
         src={fetchingImg}
         alt='fetching image'
       />
-      {isFetching ? (
-        <p id='loading-fallback'>Fetching posts...</p>
-      ) : (
-        <BlogPosts posts={fetchedPosts} />
-      )}
+      {content}
     </main>
   );
 };
